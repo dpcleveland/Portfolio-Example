0427d06f6194c27e7309049db0c7472b269de576
Daniel Cleveland <dpcleveland@gmail.com>
2016-08-05 22:39:58 -0500

    Transfer from daniel-cleveland-portfolio repository

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Grey out other pictures on mouseover
+// Call with .picture class
+$(function () {
+	var elems = $('.picture');
+	elems.on('mouseenter mouseleave', function (e) {
+		elems.not(this).stop(true).fadeTo('fast', e.type == 'mouseenter' ? 0.5 : 1);
+	});
+});
